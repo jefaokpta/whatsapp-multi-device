448b17be8e97973b9f0aee22d880f2bacd94661f
jefaokpta <jefaokpta@hotmail.com>
2022-03-02 11:40:22 -0300

    consegui conectar ao server whats

--- src/main.ts
+++ src/main.ts
@@ -20,11 +20,13 @@ async function connectToWhatsApp() {
         printQRInTerminal: true
     })
 
+    /** connection state has been updated -- WS closed, opened, connecting etc. */
     sock.ev.on('connection.update', (update) => {
         const {connection, lastDisconnect, qr} = update
         console.log('QRCODE AQUI', qr)
+        console.log('ESTADO DA CONEXAO ', connection)
         if (connection === 'close') {
-            console.log(`Conexión cerrada: ${lastDisconnect}`)
+            console.log(`CONEXAO FECHADA: ${lastDisconnect}`)
             const shouldReconnect = (lastDisconnect?.error as Boom)?.output?.statusCode !== DisconnectReason.loggedOut
             console.log('conexao fechada por ', lastDisconnect?.error, 'DATA-HORA ', lastDisconnect?.date, ', reconnecting ', shouldReconnect)
             // reconnect if not logged out
@@ -39,13 +41,57 @@ async function connectToWhatsApp() {
 
     sock.ev.on('messages.upsert', async m => {
         console.log(JSON.stringify(m, undefined, 2))
+        const message  = m.messages[0]
+        if(!message.key.fromMe){
+            console.log('respondendo para ', m.messages[0].key.remoteJid)
+            sock.sendMessage(m.messages[0].key.remoteJid!, {text: 'Faaala José!'})
+        }
 
-        console.log('respondendo para ', m.messages[0].key.remoteJid)
-        await sock.sendMessage(m.messages[0].key.remoteJid!, {text: 'Faaala José!'})
     })
 
-    // listen for when the auth credentials is updated
+    /** auth credentials updated -- some pre key state, device ID etc. */
     sock.ev.on('creds.update', saveState)
+    // ATUALIZACAO DE STATUS DA MSG
+    sock.ev.on('messages.update', m => {
+        console.log('RECEBENDO messages.update')
+        console.log(m)
+    })
+
+    // EVENTOS DISPONIVEIS
+    /** set chats (history sync), chats are reverse chronologically sorted */
+    sock.ev.on('chats.set', item => {
+        console.log('RECEBENDO chats.set')
+        console.log(`recv ${item.chats.length} chats (is latest: ${item.isLatest})`)
+    })
+    /** set messages (history sync), messages are reverse chronologically sorted */
+    sock.ev.on('messages.set', item => {
+        console.log('RECEBENDO messages.set')
+        console.log(`recv ${item.messages.length} messages (is latest: ${item.isLatest})`)
+    })
+    /** set contacts (history sync) */
+    sock.ev.on('contacts.set', item => {
+        console.log('RECEBENDO contacts.set')
+        console.log(`recv ${item.contacts.length} contacts`)
+    })
+
+    sock.ev.on('message-receipt.update', m => {
+        console.log('RECEBENDO message-receipt.update')
+        console.log(m)
+    })
+    /** INUTIL POR ENQUANTO
+    sock.ev.on('presence.update', m => {
+        console.log('RECEBENDO presence.update')
+        console.log(m)
+    })
+     */
+    sock.ev.on('chats.update', m => {
+        console.log('RECEBENDO chats.update')
+        console.log(m)
+    })
+    sock.ev.on('contacts.upsert', m => {
+        console.log('RECEBENDO contacts.upsert')
+        console.log(m)
+    })
 }
 // run in main file
 connectToWhatsApp()
---
+++ src/static/staticVar.ts
@@ -0,0 +1,9 @@
+/** DEVELOPMENT */
+export const urlBase = 'http://localhost:8080'
+export const mediaFolder = 'whatsMedia'
+
+/** DOCKER CONTAINER */
+// export const mediaFolder = '/whatsMedia'
+// export const urlBase = 'http://192.168.15.152:8080' // LOCAL
+// export const urlBase = 'http://128.0.0.1:9090' // HOMOLOGATION
+// export const urlBase = 'http://128.0.0.1:8080' // PRODUCTION
