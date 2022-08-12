6f0ba276bff9a41e38847ca54a02db9d7dc312df
jefaokpta <jefaokpta@hotmail.com>
2022-08-12 14:38:48 -0300

    vou atualizar o baileys pra ver se muda algo

--- src/static/staticVar.ts
+++ src/static/staticVar.ts
@@ -1,11 +1,11 @@
 /** DEVELOPMENT */
-// export const urlBase = 'http://localhost:8080'
-// export const mediaFolder = 'whatsMedia'
+export const urlBase = 'http://localhost:8080'
+export const mediaFolder = 'whatsMedia'
 
 /** DOCKER CONTAINER */
-export const mediaFolder = '/whatsMedia'
+// export const mediaFolder = '/whatsMedia'
 // export const urlBase = 'http://192.168.15.152:8080' // LOCAL
-export const urlBase = 'http://128.0.0.1:9090' // HOMOLOGATION
+// export const urlBase = 'http://128.0.0.1:9090' // HOMOLOGATION
 // export const urlBase = 'http://128.0.0.1:8080' // PRODUCTION
 
 /** ATENCAO NAO ESQUECA DO COMANDO TSC!!! */
