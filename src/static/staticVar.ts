70576f2d019d9ea9bd7add13e3c23b7f9aa22608
jefaokpta <jefaokpta@hotmail.com>
2022-11-29 15:13:08 -0300

    usando gw default pra comunicar com host

--- src/static/staticVar.ts
+++ src/static/staticVar.ts
@@ -1,11 +1,11 @@
 /** DEVELOPMENT */
-export const urlBase = 'http://localhost:8080'
-export const mediaFolder = 'whatsMedia'
+// export const urlBase = 'http://localhost:8080'
+// export const mediaFolder = 'whatsMedia'
 
 /** DOCKER CONTAINER */
-// export const mediaFolder = '/whatsMedia'
+export const mediaFolder = '/whatsMedia'
 // export const urlBase = 'http://192.168.15.152:8080' // LOCAL CASA
 // export const urlBase = 'http://192.168.15.36:8080' // LOCAL WORK
-// export const urlBase = 'http://128.0.0.1:8080' // PRODUCTION
+export const urlBase = 'http://172.17.0.1:8080' // PRODUCTION
 
 /** ATENCAO NAO ESQUECA DO COMANDO TSC!!! */
