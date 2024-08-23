3cbd09d5b14c14f161ee4d1b045fd2b38279e24d
jefaokpta <jefaokpta@hotmail.com>
2024-08-23 11:19:31 -0300

    atualizado versao WAWEB BEEEETA!

--- src/static/versionWaWeb.ts
+++ src/static/versionWaWeb.ts
@@ -1,5 +1,5 @@
 import {WAVersion} from "@adiwajshing/baileys";
 
 export class VersionWaWeb {
-    static version: WAVersion = [2, 2412, 51] // todo: botar essa porra em variavel de ambiente
+    static version: WAVersion = [2, 3000, 1015920675] // todo: botar essa porra em variavel de ambiente
 }
